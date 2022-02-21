73bbc6e3c8fc8fe8e1fb1e33da68b872a6e7b75e
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-02-21 11:13:00 -0800

    added save tab no event listener yet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
